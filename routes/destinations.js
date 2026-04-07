d811a8179d057e9b47ada28dd0d443d621f3d843
erin.major <erinmajor254@gmail.com>
2026-04-06 18:24:21 -0600

    Tweaks for things to work

--- routes/destinations.js
+++ routes/destinations.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const DestinationController = require('../controllers/destinationController');
 
 router.get('/', DestinationController.getAllDestinations);
-router.get('/:id', DestinationController.getDestinationById);
 router.get('/findByCostRange', DestinationController.findByCostRange);
+router.get('/:id', DestinationController.getDestinationById);
 router.post('/', DestinationController.createDestination);
 router.put('/:id', DestinationController.updateDestination);
 router.delete('/:id', DestinationController.deleteDestination);
