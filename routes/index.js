3c593280dc1b03b4cb5458ea22861ebffc457db0
erin.major <erinmajor254@gmail.com>
2026-04-09 20:16:24 -0600

    Remove index imports

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,4 @@ router.get('/', (req, res) => {
     res.send(`Welcome to the Vacations API! Navigate to /api-docs for API documentation.`);
 });
 
-// 2. Import all the individual route files
-const users = require('./users');
-const destinations = require('./destination'); 
-const packages = require('./package');
-const reviews = require('./review');
-
-// 3. Assign the paths
-router.use('/users', users);             // Glenn's part
-router.use('/destinations', destinations); // Erin's part
-router.use('/packages', packages);       // Allison's part? Please confirm
-router.use('/reviews', reviews);         // Juan's part
-
 module.exports = router;
